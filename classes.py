1f4c9a79a1a2c155baf81c09da17681177e6c564
Hannah Hudson <hannah.hch@gmail.com>
2017-11-05 09:49:23 -0500

    added a greeter class

--- classes.py
+++ classes.py
@@ -1,1 +1,15 @@
+#first argument is always "self", a bit like "this"
 class Greeter:
+    def __init__(self, greeting):
+        self.greeting = greeting
+
+    def greet(self, name):
+        print(self.greeting + ", " + name + "!")
+
+#JS example: new Greeter("Hello")
+#Ruby example: Greeter.new("Hello")
+
+english_greeter = Greeter("Hello")
+spanish_greeter = Greeter("Hola")
+
+spanish_greeter.greet("Remy")
