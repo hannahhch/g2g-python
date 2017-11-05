be91192fdf2e297d9d7a7d384dc7cf7aa98601dd
Hannah Hudson <hannah.hch@gmail.com>
2017-11-05 10:43:53 -0500

    added a lists file

---
+++ lists.py
@@ -0,0 +1,13 @@
+my_list = ["banana", 17, ["a", "b"], 9.5]
+
+print(my_list[0:3])
+print(my_list[-1])
+print(my_list[0:4:2])
+print(my_list[::-1])
+
+short_list = ["hi",
+              "hello",
+              "hola",
+              "bonjour",
+              ]
+print(short_list)
