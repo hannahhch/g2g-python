b7eef56f8073276082e0c2c37d6c566699a47d13
Hannah Hudson <hannah.hch@gmail.com>
2017-11-05 10:15:27 -0500

    renamed a file, imported a file to another

---
+++ imports.py
@@ -0,0 +1,14 @@
+#import datetime
+
+#print(datetime.date.today())
+
+#1/3 and 7/9 do the same
+
+from datetime import date
+
+print(date.today())
+
+import greeter
+
+en_greeter = greeter.Greeter()
+en_greeter.greet("class")
