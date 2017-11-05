a50bedc1fb25bf4836e7997a701f2d8f0ae77674
Hannah Hudson <hannah.hch@gmail.com>
2017-11-05 10:06:53 -0500

    triple quoted string ex

--- classes.py
+++ classes.py
@@ -1,15 +1,35 @@
 #first argument is always "self", a bit like "this"
 class Greeter:
-    def __init__(self, greeting):
+    """
+    The Greeter class privdes a multi-lingual way to greet people.
+    """
+    def __init__(self, greeting="Hello", excited=True):
         self.greeting = greeting
+        self.excited = excited
 
     def greet(self, name):
-        print(self.greeting + ", " + name + "!")
+        punctuation = "."
+        if self.excited:
+            punctuation = "!"
+        print(self.greeting + ", " + name + punctuation)
+
+
 
 #JS example: new Greeter("Hello")
 #Ruby example: Greeter.new("Hello")
 
-english_greeter = Greeter("Hello")
+english_greeter = Greeter()
 spanish_greeter = Greeter("Hola")
+french_greeter = Greeter(excited=False, greeting="Bonjour")
 
+english_greeter.greet("Dorian")
 spanish_greeter.greet("Remy")
+french_greeter.greet("Kerry")
+
+# multi-line string example
+output = """
+Rowan says, "Hello!"
+
+Sawyer returns the greeting.
+"""
+print(output)
