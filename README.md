a6f82995b308806d0a51279052149eb7946d2cc0
Hannah Hudson <hannah.hch@gmail.com>
2017-11-05 10:32:10 -0500

    readme and created a comprehension

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a practice repo created at a one day Python/Django workshop with Clinton Dreisbach and Green To Go.
--- comprehensions.py
+++ comprehensions.py
@@ -1,1 +1,23 @@
-#comprehensions are like map or filter 
+#comprehensions are like map or filter
+
+words = ["file", "apple", "directory", "banana"]
+
+#strings
+for word in words:
+    print(word[::-1])
+
+#list (array)
+reverse_words = []
+for word in words:
+    reverse_words.append(word[::-1])
+
+print("reverse word list:", reverse_words)
+
+reverse_words = [words[::-1] for word in words]
+new_word_list = [word for word in words]
+
+short_word_list = [word
+                    for word in words
+                    if len(word) < 7]
+
+print("short word list: ", short_word_list)
